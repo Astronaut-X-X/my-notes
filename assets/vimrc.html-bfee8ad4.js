11d85ac9f9b297f9a23472deecf7c3d8d1650217
Astronaut-X-X <48613764+Astronaut-X-X@users.noreply.github.com>
2023-05-30 17:39:41 +0800

    Deploying to gh-pages from @ Astronaut-X-X/my-notes@bbf46396a01d932a0f2862b633da44dcf32451aa 🚀

---
+++ assets/vimrc.html-bfee8ad4.js
@@ -0,0 +1,26 @@
+import{_ as i,p as e,q as n,a1 as s}from"./framework-efe98465.js";const d={},l=s(`<h3 id="vimrc" tabindex="-1"><a class="header-anchor" href="#vimrc" aria-hidden="true">#</a> Vimrc</h3><p>.vimrc 是 Vim 文本编辑器的配置文件，用于定义用户的编辑器偏好设置和自定义快捷键等内容。</p><p>以下是一份简单的 .vimrc 示例：</p><div class="language-.vimrc line-numbers-mode" data-ext=".vimrc"><pre class="language-.vimrc"><code>&quot; 显示行号
+set number
+
+&quot; 语法高亮
+syntax on
+
+&quot; 配色方案
+colorscheme desert
+
+&quot; 设置自动缩进
+set autoindent
+
+&quot; 设置 Tab 缩进为 4 个空格
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set expandtab
+
+&quot; 显示当前行和列号
+set ruler
+
+&quot; 打开文件类型检测
+filetype on
+filetype plugin on
+filetype indent on
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>这些设置可以根据个人喜好进行修改和添加，例如添加插件支持、更改配色方案、设置保存备份等等</p><p>自定义快捷键映射：</p>`,6),v=[l];function a(r,c){return e(),n("div",null,v)}const m=i(d,[["render",a],["__file","vimrc.html.vue"]]);export{m as default};
